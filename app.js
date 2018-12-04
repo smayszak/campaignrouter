3f813e680279e38796cd7fea9455a144a61c8426
Mayszak <mayszaks@f45c898a283d.ant.amazon.com>
2018-12-04 08:16:34 -0800

    changes for re:invent

--- app.js
+++ app.js
@@ -1,61 +1,60 @@
+//bring in express- this is a fully functional webserver!
+var express = require('express');
+var app = express();
+///setup body poarser for posts//////////
+var bodyParser = require('body-parser');
+var defaultCampaignConfig = null;
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+///XRAY - Load SDK and register////////////////////////////
+var AWSXRay = require('aws-xray-sdk');
+app.use(AWSXRay.express.openSegment('CampaignLambdaDetail'));
+
+//include my custom code (aka - my budiness logic) 
+var campaignLoader = require('modules/campaigns');
+var hashring = require('modules/hashring');
+hashring.load(campaignLoader.defaultCampaign());
+
+
+/////////////app routes/////////////////////
+
+//LAMDBA POST API function handler
+app.post('/', function(req, res) {
+   //XRAY Logging additional, searchable data.\//extract input from API call
+    var id = req.body.customerId;
+  AWSXRay.captureFunc('annotations', function(subsegment){
+    //extract input from API call
+    var id = req.body.customerId;
+    //XRAY: I can add additional fields here. These will be searchable!
+    subsegment.addAnnotation('ID', id);
+    
+    //validation check
+   if(isNaN(id)){
+        res.status(500).send('Not a valid customer id');
+        subsegment.addAnnotation('ErrorMessage', "Not a valid customer id");
+   }
+   var campaign = hashring.get(id);
+   subsegment.addAnnotation('CampaignId', campaign.id);
+   //Cloudwatch equip of XRAY annotation without first tier filter support
+   console.log('customerId: ' + id + ' mapped to: ' + campaign.id);
+   //respond to our clients!
+    res.json({ campaign: campaign });
+  });
+});
+
+
+//LAMBDA: API - GET function handler
+app.get('/', function(req, res) {
+    console.log('received get to listCampaigns');
+    var data = campaignLoader.defaultCampaign();
+    res.json({ campaignList: data });
+});
+
+////Xray - close out Xray/////////////
+app.use(AWSXRay.express.closeSegment());
+
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = function(defaultCampaign) {
-    //cache configuration, from s3 call on lambda cold start
-    var DEFAULT_CAMPAIGN = defaultCampaign; 
-    var hashring = require('modules/hashring');
-    hashring.load(DEFAULT_CAMPAIGN);
-    
-    //bring in express- this is a fully functional webserver!
-    var express = require('express');
-    var app = express();
-    
-    ///setup body poarser for posts//////////
-    var bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({
-      extended: true
-    }));
-    app.use(bodyParser.json());
-    
-    /////////////app routes/////////////////////
-    ///XRAY - Load SDK and register////////////////////////////
-    var AWSXRay = require('aws-xray-sdk');
-    //to use XRAY, we need to open the segment before we register routes.
-    app.use(AWSXRay.express.openSegment('LambdaRequestRouter'));
-    
-    //Register LAMDBA POST API function handler
-    app.post('/', function(req, res) {
-        
-        //extract input from API call
-       var id = req.body.customerId;
-       if(isNaN(id)){
-            res.status(500).send('Not a valid customer id');
-       }
-       //map the customer id to a campaign in our hashring
-       var campaign = hashring.get(req.body.customerId);
-       
-       //XRAY Logging additional, searchable data.
-       AWSXRay.captureFunc('annotations', function(subsegment){
-        subsegment.addAnnotation('Body', JSON.stringify(req.body));
-        //XRAY: I can add additional fields here. These will be searchable!
-        subsegment.addAnnotation('CustomerId', req.body.customerId);
-      });
-    
-       //Anything i log to console log will appear in cloudwatch logs.
-       console.log('customerId: ' + id + ' mapped to: ' + campaign);
-       
-       //respond to our clients!
-        res.json({ campaign: campaign });
-    });
-    
-    //LAMBDA: API - GET function handler
-    app.get('/', function(req, res) {
-        console.log('received get to listCampaigns');
-        res.json({ campaignList: DEFAULT_CAMPAIGN });
-    });
-    
-    ////After registering our routes, we need to close out our XRAY segment/////////////
-    app.use(AWSXRay.express.closeSegment());
-    
-    //finally, return the app so that it can be used.
-    return app;
-}
+module.exports = app;
--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 'use strict';
 
 const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+const server = awsServerlessExpress.createServer(app)
 
-var AWS = require('aws-sdk');
-var defaultCampaignConfig = null;
-
-var s3 = new AWS.S3();
-var params = {Bucket: 'mayszak-campaign', Key: 'campaign.json'};
-var configRequest = s3.getObject(params);
-
-exports.handler = (event, context) => {
-    configRequest.on('success', function(response) {
-            var objectData = response.data.Body.toString('utf-8'); // Use the encoding necessary
-            defaultCampaignConfig = JSON.parse(objectData);
-            
-            const app = require('./app')(defaultCampaignConfig);
-            const server = awsServerlessExpress.createServer(app);
-            awsServerlessExpress.proxy(server, event, context);
-    });
-
-    configRequest.send()    
-}
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
